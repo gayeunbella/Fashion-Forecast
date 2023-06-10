de57b6586beae8481815ac553b5e48c9af1f3fbe
ethan xing <ethanxing2007@gmail.com>
2023-06-10 01:44:50 -0400

    updated

---
+++ python webscraping/webscrape.py
@@ -0,0 +1,16 @@
+from requests_html import HTMLSession
+
+s = HTMLSession()
+
+query = 'waterloo'
+url = f'https://www.google.com/search?q=weather+{query}'
+
+r = s.get(url, headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.4 Safari/605.1.15'})
+
+currenttemp = r.html.find('span.wob_t', first = True).text
+
+high = r.html.find('div.gNCp2e span.wob_t', first = True).text
+
+low =  r.html.find('div.QrNVmd.ZXCv8e span.wob_t', first = True).text
+
+print(currenttemp+"°c", high+"°c", low+"°c")
